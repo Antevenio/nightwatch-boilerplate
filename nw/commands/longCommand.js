59bd03d7424c0c603a7f314a267c09320b0a6857
jbarroso <jbarroso@antevenio.com>
2016-04-11 11:57:47 +0200

    Merge fixed

--- nw/commands/longCommand.js
+++ nw/commands/longCommand.js
@@ -3,13 +3,16 @@ const events = require('events');
 const LONG_TIME = 2000;
 
 function Cmd() {
-    events.EventEmitter.call(this);
+  events.EventEmitter.call(this);
 }
 util.inherits(Cmd, events.EventEmitter);
 
-Cmd.prototype.command = function () {
-    setTimeout(() => this.emit('complete'), LONG_TIME);
-    return this;
+Cmd.prototype.command = function() {
+  setTimeout(() => {
+    console.log("Long command Completed!!");
+    this.emit('complete');
+  }, LONG_TIME);
+  return this;
 };
 
 module.exports = Cmd;
